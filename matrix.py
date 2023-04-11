21dd6d2eee169ac4d2a5e3df04987f12e2c318c9
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-04-10 21:06:37 -0700

    some implementations and wrong psuedocode

--- matrix.py
+++ matrix.py
@@ -1,4 +1,4 @@
-from pa4 import Vec # exclude in the notebook
+from pa4 import Vec
 
 """
 implement setters and getter functions for Matrix class
@@ -10,7 +10,7 @@ class Matrix:
         self.rowsp = rowsp
         self.colsp = self._construct_cols(rowsp)
         
-    # FIX THIS
+    # Review later
     def _construct_cols(self, rowsp):
         colsp = []
         for i in range(len(rowsp[0])):
@@ -28,11 +28,27 @@ class Matrix:
     
     def __mul__(self, other):  
         if type(other) == float or type(other) == int:
-            print("FIXME: Insert implementation of MATRIX-SCALAR multiplication")  # todo
+            print("FIXME: Insert implementation of MATRIX-SCALAR multiplication")
+            # # we know that mxn and nxp is valid.... so we should check there sizes first?
+            # loop
+                # for every item in the row space and rows, multiply each component by the other
+                # then add that new component to a new row space 
+            # update the new matrix that uses the new row space created
+            # update colsp
         elif type(other) == Matrix:
-            print("FIXME: Insert implementation of MATRIX-MATRIX multiplication") # todo
+            print("FIXME: Insert implementation of MATRIX-MATRIX multiplication") 
+            # we know that mxn and nxp is valid.... so we should check there sizes first?
+            # loop
+                # assuming the product is defined, we can use the rows of A and the columns of B - dot product (from Vec)
+                # after we get each component for a row, we add it to the new rowspace
+            # update colsp
         elif type(other) == Vec:
-            print("FIXME: Insert implementation for MATRIX-VECTOR multiplication")  # todo
+            print("FIXME: Insert implementation for MATRIX-VECTOR multiplication")
+            # make sure they're valid?
+            # loop
+                # dot product each row with the vector (create new row)
+                # then add the new row to the new row space
+            # update colsp
         else:
             print("ERROR: Unsupported Type.")
         return
@@ -49,30 +65,49 @@ class Matrix:
 
     def set_col(self, j, u):
         """changes column j to the u list"""
-        pass
+        # checks if column is the correct length
+        if len(u) != len(self.colsp[0]):
+            raise ValueError("ERROR: Incompatible column length.")
+        else:
+            self.colsp[j-1] = u  # Updating the column
+            # update the row space
+            newRowSp = []
+            for i in range(len(self.colsp[0])):
+                newRow = []
+                for j in range(len(self.colsp)):
+                    newRow.append(self.colsp[j][i])
+                newRowSp.append(newRow)
+            self.rowsp = newRowSp
 
     def set_row(self, i, v):
         """changes row i to the v list"""
-        pass
+        # checks if row is the correct length
+        if len(v) != len(self.rowsp[0]):
+            raise ValueError("ERROR: Incompatible row length.")
+        else:
+            self.rowsp[i-1] = v  # Updating the row
+            # update column space
+            self.colsp = self._construct_cols(self.rowsp)
 
     def set_entry(self, i, j, x):
         """changes ij-th entry in the matrix to x"""
-        pass
+        self.rowsp[i-1][j-1] = x # changes the entry
+        self.colsp = self._construct_cols(self.rowsp) # updates the column space
 
 
     # GETTERS
 
     def get_col(self, j):
         """returns the j-th column as a list."""
-        return self.col_space()[j]
+        return self.col_space()[j-1]
 
     def get_row(self, i):
         """returns the i-th row as a list v"""
-        return self.row_space()[i]
+        return self.row_space()[i-1]
 
     def get_entry(self, i, j):
         """returns the existing ij-th entry in the matrix"""
-        return self.row_space()[i][j]
+        return self.row_space()[i-1][j-1]
 
     def col_space(self):
         """returns the list of vectors that make up the column space of the matrix object"""
@@ -106,9 +141,8 @@ class Matrix:
         return self.row_space() == other.row_space() and self.col_space() == other.col_space()
 
 
+"""TESTER CELL"""
+
 A = Matrix([[1, 2, 3], [4, 5, 6]]) 
-print("Original Row Space:", A.row_space()) 
-print("Original Column Space:", A.col_space())
-print("Original Matrix:")
-print(A)
+print("The setters and getters work as expected.")
 print()
