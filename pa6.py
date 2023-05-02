51f0b10cb80c3e02a366b2c4cc81b2528ccb18c4
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-05-02 10:59:23 -0700

    stupidest error ever

--- pa6.py
+++ pa6.py
@@ -114,7 +114,7 @@ class Matrix:
         
     def _construct_cols(self, rowsp):
         colsp = []
-        for i in range(len(rowsp[0].elements)):
+        for i in range(len(rowsp[0])):
             col = []
             for j in range(len(rowsp)):
                 col.append(rowsp[j][i])
