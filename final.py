f6116f97e0df7fa4d17ebd63631bc25b29d8ff68
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-05-03 22:41:52 -0700

    Amazing

---
+++ final.py
@@ -0,0 +1,21 @@
+import pa6
+from random import randintd
+
+def solve_QR(A, b):
+    # todo
+    pass
+  
+"""TESTER CELL"""
+n = randint(3, 5)
+
+rowsp = [[randint(-30, 30) for i in range(n)] for j in range(n)]
+while not pa6.is_independent([pa6.Vec(row) for row in rowsp]):
+    rowsp = [[randint(-30, 30) for i in range(n)] for j in range(n)]
+    
+A = pa6.Matrix(rowsp)
+x_true = pa6.Vec([randint(-10, 10) for i in range(n)])
+b = A * x_true
+print("Expected:", x_true)
+
+x = solve_QR(A, b)
+print("Returned:", x)
