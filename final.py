ec15ff4058c08b44af3bc3ce114ffe528d348263
steph lee <40439815+disappointment1738@users.noreply.github.com>
2023-05-09 22:10:11 -0700

    very wrong debugging

--- final.py
+++ final.py
@@ -2,49 +2,46 @@ import pa6
 from random import randint
 
 def solve_QR(A, b):
-    aSet = set(pa6.Vec(row) for row in A.row_space())
+    # make A a set
+    aSet = [pa6.Vec(row) for row in A.rowsp]
     # create Q
-    u = pa6.gram_schmidt(aSet) # throws error for A (type matrix has no len), but throws error when I try to use aSet (in construct cols???)
-    Q = pa6.Matrix(list(u))
+    u = pa6.gram_schmidt(aSet)
+    Q = pa6.Matrix([ui.elements for ui in u])
     R_rowsp = []
-
     # Use col(A) and col(Q) to computer R where every elm is <aj, ui>
     # note both A and Q are the same size
     for i in range(len(Q.rowsp)):
         row = []
         for j in range(len(Q.colsp)):
+            elm = 0
             if i <= j:
-                elm = Q.rowsp[i] * A.rowsp[j] 
+                elm = pa6.Vec(Q.rowsp[i]) * pa6.Vec(A.colsp[j])
             else:
                 elm = 0
             row.append(elm)
             # add vector to R's rowspace
         row = pa6.Vec(row)
         R_rowsp.append(row)
-    R = pa6.Matrix(R_rowsp) # nxn matrix
+    R = pa6.Matrix([row.elements for row in R_rowsp]) # nxn matrix
 
     # Transpose Q
-    temp_qT_rowsp = Q.rowsp.copy()
-    listQT = [temp_qT_rowsp] # temp list
-    listQ = [Q.rowsp] 
-    qT = []
-    for i in range(len(Q.rowsp)):
-        for j in range(len(Q.colsp)):
-            listQT[j][i] = listQ[i][j]
-        qT.append(pa6.Vec(listQT[i]))
-    Q_transpose = pa6.Matrix(qT) # mxn matrix
+    QTrowsp = []
+    for i in range(len(Q.colsp)):
+        row = []
+        for j in range(len(Q.rowsp)):
+            row.append(Q.rowsp[j][i])
+        QTrowsp.append(row) 
+    Q_transpose = ([row.elements for row in QTrowsp]) # mxn matrix
 
     # Use backwards substitution to solve R * x = Q_transpose * b
     n = R.rowsp # num of rows in R
     x = [0 for i in range(len(n))] # create "vector" of unknowns
     c = Q_transpose * b # product of matrix-vector mult.
-    cList = [c] 
-    rList = [R.rowsp]
     for i in range(n-1, -1, -1):
         s = 0 # temp variable, helps calculate num of remaining unknowns
         for j in range(i+1, n):
-            s += rList[i][j] * x[j]
-        x[i] = (cList[i]-s) / rList[i][i]
+            s += R.rowsp[i][j] * x[j]
+        x[i] = (c.elements[i]-s) / R.rowsp[i][i]
     x = pa6.Vec(x)
     return x
 
